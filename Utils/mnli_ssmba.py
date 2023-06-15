dab8e283f4fdd3ba1ac862814cd5a8889da1227b
Violeta Chatalbasheva <V.P.Chatalbasheva-1@student.tudelft.nl>
2023-06-15 11:29:39 +0200

    Clean up code.

--- Utils/mnli_ssmba.py
+++ Utils/mnli_ssmba.py
@@ -5,9 +5,28 @@ train_df = pd.read_json("../Datasets/multinli_1.0/multinli_1.0/multinli_1.0_trai
 val_df = pd.read_json("../Datasets/multinli_1.0/multinli_1.0/multinli_1.0_dev_matched.jsonl", lines=True)
 test_df = pd.read_json("../Datasets/multinli_1.0/multinli_1.0/multinli_1.0_dev_mismatched.jsonl", lines=True)
 
-# train_genre_df = train_df['genre']
-# val_genre_df = val_df['genre']
-# test_genre_df = test_df['genre']
+
+def count_data_splits():
+    train_genre_df = train_df['genre']
+    val_genre_df = val_df['genre']
+    test_genre_df = test_df['genre']
+
+    dataset = [train_genre_df, val_genre_df, test_genre_df]
+    splits = ["train", "test", "val"]
+
+    # Get number of examples for each genre
+    genre_names = ['fiction', 'government', 'slate', 'telephone', 'travel', 'facetoface', 'letters', 'nineeleven', 'oup', 'verbatim']
+
+    # Count the number of genre examples fpt train, val and test split
+    splits_genre_counts = {}
+    for i, data in enumerate(dataset):
+        genre_counts = {g: 0 for g in genre_names}
+        for g in data:
+            genre_counts[g] += 1
+        splits_genre_counts[splits[i]] = genre_counts
+
+
+train, val, test = {}, {}, {}
 
 # Define label mapping
 label_mapping = {
@@ -16,36 +35,10 @@ label_mapping = {
     'neutral': 2
 }
 
-# dataset = [train_genre_df, val_genre_df, test_genre_df]
-splits = ["train", "test", "val"]
-
-# Get number of examples for each genre
-genre_names = ['fiction', 'government', 'slate', 'telephone', 'travel', 'facetoface', 'letters', 'nineeleven', 'oup', 'verbatim']
-
 # In-domain genres
 genre_matched = ['fiction', 'government', 'slate', 'telephone', 'travel']
 # Out-of-domain genres
 genre_mismatched = ['facetoface', 'letters', 'nineeleven', 'oup', 'verbatim']
-
-
-# Count the number of genre examples fpt train, val and test split
-# splits_genre_counts = {}
-# for i, data in enumerate(dataset):
-#     genre_counts = {genre: 0 for genre in genre_names}
-#     for genre in data:
-#         genre_counts[genre] += 1
-#     splits_genre_counts[splits[i]] = genre_counts
-
-
-# Avoid biasing the data
-train, val, test = {}, {}, {}
-
-for genre in genre_matched:
-    train[genre] = {}
-    val[genre] = {}
-
-for genre in genre_mismatched:
-    test[genre] = {}
 
 
 # For each genre, separate the sentences and labels
@@ -54,55 +47,28 @@ def group_genres(df, genre, data_dict):
     data_dict[genre]['label'] = [label_mapping[row['gold_label']] for _, row in df.loc[df['genre'] == genre].iterrows() if row['gold_label'] != '-']
 
 
-# Combine premise and hypothesis
-# Map labels to integers
-for genre in genre_matched:
-    group_genres(train_df, genre, train)
-    group_genres(val_df, genre, val)
+def process_mnli(split, genre, split_df, new_df, num_samples):
+    split[genre] = {}
+    group_genres(split_df, genre, split)
+    split[genre]['text'] = split[genre]['text'][:num_samples]
+    split[genre]['label'] = split[genre]['label'][:num_samples]
+    new_df_1 = pd.DataFrame(split[genre], columns=['text', 'label'], index=None)
+    return pd.concat([new_df, new_df_1])
 
-for genre in genre_mismatched:
-    group_genres(test_df, genre, test)
 
 # Combine text with label per genre and get the corresponding number of examples for each split
 new_train_df = pd.DataFrame()
 new_val_df = pd.DataFrame()
 for genre in genre_matched:
-    train[genre]['text'] = train[genre]['text'][:100]
-    train[genre]['label'] = train[genre]['label'][:100]
-    new_train_df_1 = pd.DataFrame(train[genre], columns=['text', 'label'], index=None)
-    new_train_df = pd.concat([new_train_df, new_train_df_1])
-    val[genre]['text'] = val[genre]['text'][:200]
-    val[genre]['label'] = val[genre]['label'][:200]
-    new_val_df_1 = pd.DataFrame(val[genre], columns=['text', 'label'], index=None)
-    new_val_df = pd.concat([new_val_df, new_val_df_1])
+    new_train_df = process_mnli(train, genre, train_df, new_train_df, 100)
+    new_val_df = process_mnli(val, genre, val_df, new_val_df, 200)
 
 
 new_test_df = pd.DataFrame()
 for genre in genre_mismatched:
-    test[genre]['text'] = test[genre]['text'][:200]
-    test[genre]['label'] = test[genre]['label'][:200]
-    new_test_df_1 = pd.DataFrame(test[genre], columns=['text', 'label'], index=None)
-    new_test_df = pd.concat([new_test_df, new_test_df_1])  # , ignore_index=True
-
-print(len(new_train_df))
-print(len(new_val_df))
-print(len(new_test_df))
-# # Select only the 'text' and 'label' columns
-# new_df = train_df[['text', 'label']]
+    new_test_df = process_mnli(test, genre, test_df, new_test_df, 200)
 
 # # Save DataFrame to CSV
-# new_df.to_csv('mnli_train.csv', index=False)
-
-# for genre in genre_matched:
-#     print(len(train[genre]['text']))
-#     print(len(train[genre]['label']))
-#
-# print()
-# for genre in genre_matched:
-#     print(len(val[genre]['text']))
-#     print(len(val[genre]['label']))
-#
-# print()
-# for genre in genre_mismatched:
-#     print(len(test[genre]['text']))
-#     print(len(test[genre]['label']))
+new_train_df.to_csv('../Datasets/MNLI_ssmba_train.csv', index=False, header=False)
+new_val_df.to_csv('../Datasets/MNLI_ssmba_val.csv', index=False, header=False)
+new_test_df.to_csv('../Datasets/MNLI_ssmba_test.csv', index=False, header=False)
