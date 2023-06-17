82d99b9c0b7b4189668083699b52078e48c01372
Robin Gerster <31821147+RobinGerster@users.noreply.github.com>
2023-06-17 15:26:36 +0200

    combined plots

--- Examples/plot.py
+++ Examples/plot.py
@@ -12,21 +12,36 @@ approach = "SSMBA"
 conditions = ["bias", "no_bias"]
 models = ["BERT", "LSTM", "RNN"]
 naugs = [1, 2, 4, 8, 16, 32]
-types = ["id"]  #Also use ood but put plots in appendix I think
+types = ["id"]  # Also use ood but put plots in appendix
 
-# Loop over conditions
-for i, cond in enumerate(conditions):
-    for j, typ in enumerate(types):
-        plt.figure(figsize=(10, 6))  # create a new figure for each graph
-        for model in models:
+plt.figure(figsize=(10, 6))  # create a single figure for both conditions
+
+# Define colors for each model
+colors = ['blue', 'red', 'green']  # Add more colors if needed
+
+# Loop over models
+for i, model in enumerate(models):
+    # Get the color for the model
+    color = colors[i % len(colors)]
+
+    # Loop over conditions
+    for j, cond in enumerate(conditions):
+        for k, typ in enumerate(types):
             mean_accuracy = [np.mean(results[model][cond][naug][typ]) for naug in naugs]
-            plt.plot(naugs, mean_accuracy, marker="o", markersize=5, label=model)
 
-        plt.xlabel('Number of Augments')
-        plt.ylabel('Mean Accuracy')
-        # Adjust title depending on the condition
-        condition_name = "Naive" if cond == "no_bias" else "Biased"
-        plt.title(f'{dataset}: {approach} and {condition_name} {typ} 32 Token Window Performance')
-        plt.legend()
+            # Determine the marker type based on the condition
+            marker = 'o' if cond == "no_bias" else 's'
 
-        plt.show()
+            # Plot data with lines and markers, and label
+            c = "Biased"
+            if cond == "no_bias":
+                c = "Naive"
+            plt.plot(naugs, mean_accuracy, marker=marker, color=color, label=f'{model} ({c})')
+
+plt.xlabel('Number of Augments', fontsize=18)
+plt.ylabel('Mean Accuracy', fontsize=18)
+plt.title(f'{dataset}: {approach} 32 Token Window Performance', fontsize=18)
+plt.legend()
+plt.show()
+
+
